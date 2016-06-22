027449a007fcddbb9a5033e0401eb996db22d583
Michael <michaeldrotar@gmail.com>
2016-06-21 23:54:17 -0400

    Make rubocop happy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,10 @@
-require File.join(Rails.root,'lib','openshift_secret_generator.rb')
+require File.join(Rails.root, 'lib', 'openshift_secret_generator.rb')
 
 # Be sure to restart your server when you modify this file.
 
 # Set token based on intialize_secret function (defined in initializers/secret_generator.rb)
 
-Rails.application.config.session_store :cookie_store, :key => initialize_secret(
+Rails.application.config.session_store :cookie_store, key: initialize_secret(
   :session_store,
   '_railsapp_session'
 )
