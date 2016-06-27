cb16f3b3b41654d5abf0ed21f8371c3d4b56e698
Michael <michaeldrotar@gmail.com>
2016-06-27 10:06:32 -0400

    Remove openshift stuff

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,1 @@
-require File.join(Rails.root, 'lib', 'openshift_secret_generator.rb')
-
-# Be sure to restart your server when you modify this file.
-
-# Set token based on initialize_secret function
-
-Rails.application.config.session_store :cookie_store, key: initialize_secret(
-  :session_store,
-  '_railsapp_session'
-)
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# RailsApp::Application.config.session_store :active_record_store
+config.session_store :cookie_store
