6643a45c00d01889c9c6f9e81639288b1316c668
Michael <michaeldrotar@gmail.com>
2016-06-27 09:10:17 -0400

    Update deprecated rails config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,8 +18,8 @@ Rails.application.configure do
   # like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Disable Rails's static file server (Apache or nginx will already do this).
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
