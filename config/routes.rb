dc42d6013a38d5ce57a4d07ac4708172cb16d03a
Michael <michaeldrotar@gmail.com>
2016-07-01 17:01:56 -0400

    Add sign in/out links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   locale_constraint = /#{I18n.available_locales.join('|')}/
   scope '/:locale' do
-    devise_for :users
+    devise_for :users, path: ''
     get '/orpheus', to: 'orpheus#index'
     namespace :orpheus do
       resources :heroes, only: [:index, :show]
