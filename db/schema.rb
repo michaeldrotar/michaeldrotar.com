2f3975023d1f9fd7f0dd31647999de50d1837155
Michael <michaeldrotar@gmail.com>
2017-02-11 10:16:54 -0500

    Remove old tables from db schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,24 +16,6 @@ ActiveRecord::Schema.define(version: 20160701211222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "shop_item_translations", force: :cascade do |t|
-    t.integer  "shop_item_id", null: false
-    t.string   "locale",       null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "name"
-    t.text     "description"
-  end
-
-  add_index "shop_item_translations", ["locale"], name: "index_shop_item_translations_on_locale", using: :btree
-  add_index "shop_item_translations", ["shop_item_id"], name: "index_shop_item_translations_on_shop_item_id", using: :btree
-
-  create_table "shop_items", force: :cascade do |t|
-    t.decimal  "cost"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
