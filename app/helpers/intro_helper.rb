5a899144fc92cad26f897f3f562d4b3938805a74
Michael <michaeldrotar@gmail.com>
2017-04-17 20:16:35 -0400

    Fix font scaling, tweak intro text, add container spacing

--- app/helpers/intro_helper.rb
+++ app/helpers/intro_helper.rb
@@ -7,7 +7,7 @@ module IntroHelper
   def intro_text(text)
     characters = text.split('').map do |character|
       @count += 1
-      "<span class=\"intro__char intro__char--#{@count}\">#{character}</span>"
+      "<span class=\"intro-char intro-char--#{@count}\">#{character}</span>"
     end
     characters.join.html_safe
   end
