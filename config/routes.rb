63d002359b643147116c33bd1f5dabc4c1b81981
Michael <michaeldrotar@gmail.com>
2017-04-22 19:04:23 -0400

    Add projects index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :orpheus do
       resources :heroes, only: [:index, :show]
     end
+    resources :projects
     get '/pageboy', to: 'pageboy#index'
     get '/:section/:page', to: 'static#dispatcher', as: :static_section
     get '/:page', to: 'static#dispatcher', as: :static
