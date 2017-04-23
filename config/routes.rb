c4ae1181a80ff6c5f7766d9910987709e62ef104
Michael <michaeldrotar@gmail.com>
2017-04-23 14:53:26 -0400

    Remove locale from urls, no real need to translate my personal portfolio site, was mostly a learning exercise in how to do it

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  scope '/:locale' do
-    devise_for :users, path: '', skip: :omniauth_callbacks
-    get '/orpheus', to: 'orpheus#index'
-    namespace :orpheus do
-      resources :heroes, only: [:index, :show]
-    end
-    resources :projects, only: [:index]
-    get '/pageboy', to: 'pageboy#index'
-    get '/:section/:page', to: 'static#dispatcher', as: :static_section
-    get '/:page', to: 'static#dispatcher', as: :static
-    get '/', to: 'static#intro', defaults: { page: 'intro' }, as: :intro
+  get '/orpheus', to: 'orpheus#index'
+  namespace :orpheus do
+    resources :heroes, only: [:index, :show]
   end
+  resources :projects, only: [:index]
+  get '/pageboy', to: 'pageboy#index'
+  get '/:section/:page', to: 'static#dispatcher', as: :static_section
+  get '/:page', to: 'static#dispatcher', as: :static
+  devise_for :users, path: '', skip: :omniauth_callbacks
   devise_for :users, path: '', only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
-  root to: redirect("/#{I18n.default_locale}")
+  root to: 'static#intro', defaults: { page: 'intro' }
 end
