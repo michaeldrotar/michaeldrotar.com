7b4b213da0c10fe7ad05edd6ca45acf923da1652
Michael <michaeldrotar@gmail.com>
2018-07-02 20:15:38 -0400

    Move server to be just the web server

--- web-server.js
+++ web-server.js
@@ -8,10 +8,10 @@ const app = express();
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 // Catch all routes and redirect to the index file
 app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 // Create default port to serve the app on
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port);
 // Log to feedback that this is actually running
 console.log('Server started on port ' + port); // eslint-disable-line no-console
