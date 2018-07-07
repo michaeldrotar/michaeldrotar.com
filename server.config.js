fd3b7b85239f98fe4cea00a9ebdef641434e8403
Michael <michaeldrotar@gmail.com>
2018-07-06 22:34:26 -0400

    Fix __dirname, maybe?

--- server.config.js
+++ server.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: './server/index.js',
   target: 'node',
+  node: {
+    __dirname: false
+  },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname),
