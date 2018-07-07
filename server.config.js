3eb7867f02b28322768a9b0d2701aafe5401481e
Michael <michaeldrotar@gmail.com>
2018-07-07 08:11:18 -0400

    Add server sourcemaps

--- server.config.js
+++ server.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: env,
   entry: './server/index.js',
   target: 'node',
   node: {
     __dirname: false
   },
+  devtool: env == 'development' ? 'inline-source-map' : false,
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname),
