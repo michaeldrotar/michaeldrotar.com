38552ec03da227bac7e4676e919729216f8449bf
Michael <michaeldrotar@gmail.com>
2019-01-03 08:27:00 -0500

    Add logo to header

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,26 +1,25 @@
 <template>
-  <header class="mdc-top-app-bar">
+  <header class="mdc-top-app-bar mdc-top-app-bar--fixed">
     <div class="mdc-top-app-bar__row">
       <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-        <a href="#" class="material-icons mdc-top-app-bar__navigation-icon">menu</a>
-        <span class="mdc-top-app-bar__title">Title</span>
+        <span class="mdc-top-app-bar__title">michaeldrotar.com</span>
       </section>
-      <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+      <!--section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
         <app-header-action to="projects" icon-id="group_work" text="Projects" />
-      </section>
+      </section-->
     </div>
   </header>
 </template>
 
 <script>
 import { MDCTopAppBar } from '@material/top-app-bar';
-import AppHeaderAction from '@/components/app-header-action';
+// import AppHeaderAction from '@/components/app-header-action';
 
 export default {
   name: 'mdc-top-app-bar',
   foo: 'bar',
   components: {
-    AppHeaderAction
+    // AppHeaderAction
   },
   mounted: function() {
     this.mdcTopAppBar = new MDCTopAppBar(this.$el);
@@ -30,4 +29,31 @@ export default {
 
 <style lang="scss">
 @import '@material/top-app-bar/mdc-top-app-bar';
+
+.mdc-top-app-bar {
+  @include mdc-top-app-bar-ink-color($body-text);
+  @include mdc-top-app-bar-icon-ink-color($body-text);
+  @include mdc-top-app-bar-fill-color($body-bg);
+
+  background-image: url('../assets/logo.png');
+  background-position: -50px -100px;
+  background-repeat: no-repeat;
+  background-size: 200px;
+}
+
+.mdc-top-app-bar::before {
+  content: '';
+
+  background-image: url('../assets/logo.png');
+  background-position: -50px -156px;
+  background-repeat: no-repeat;
+  background-size: 200px;
+
+  display: block;
+  height: 10px;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+}
 </style>
