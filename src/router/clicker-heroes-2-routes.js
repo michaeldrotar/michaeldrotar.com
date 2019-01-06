88bf79f720eadc4353a9c58253e05660dd0f0dbb
Michael <michaeldrotar@gmail.com>
2019-01-06 10:53:20 -0500

    Re-organize ch2 files

--- src/router/clicker-heroes-2-routes.js
+++ src/router/clicker-heroes-2-routes.js
@@ -1,7 +1,7 @@
 import ClickerHeroes2Layout from '@/layouts/clicker-heroes-2-layout';
 import ClickerHeroes2Page from '@/pages/clicker-heroes-2-page';
-import ClickerHeroes2ModsPage from '@/pages/clicker-heroes-2-mods-page';
-import ClickerHeroes2FrequentRubyShopModPage from '@/pages/clicker-heroes-2-frequent-ruby-shop-mod-page';
+import ClickerHeroes2ModsPage from '@/pages/clicker-heroes-2/mods-page';
+import ClickerHeroes2FrequentRubyShopModPage from '@/pages/clicker-heroes-2/mods/frequent-ruby-shop-page';
 
 export default [
   {
