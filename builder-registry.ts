36416ccfddc1437c700eda3e958b1ec7cba21076
michaeldrotar <michaeldrotar@gmail.com>
2024-02-08 14:39:24 -0500

    Force builder api key existence

--- builder-registry.ts
+++ builder-registry.ts
@@ -2,7 +2,7 @@
 import { builder, Builder } from '@builder.io/react'
 import Counter from './components/Counter/Counter'
 
-builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY)
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!)
 
 Builder.registerComponent(Counter, {
   name: 'Counter',
