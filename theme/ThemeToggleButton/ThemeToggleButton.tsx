5b2057ec6f0063b571063ccbc15876ce6dcbc22b
michaeldrotar <michaeldrotar@gmail.com>
2024-02-09 22:44:15 -0500

    Finish theme toggle button

--- theme/ThemeToggleButton/ThemeToggleButton.tsx
+++ theme/ThemeToggleButton/ThemeToggleButton.tsx
@@ -1,47 +1,51 @@
 'use client'
 
 import { MoonIcon, SunIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
 import { useTheme } from 'next-themes'
 
 const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent'
 
 export function ThemeToggleButton() {
-  const { theme, systemTheme, setTheme, themes, forcedTheme, resolvedTheme } =
-    useTheme()
-  console.log({ theme, systemTheme, themes, forcedTheme, resolvedTheme })
+  const { theme, systemTheme, setTheme, resolvedTheme } = useTheme()
+
   if (!theme) {
     return (
       <div
-        className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
-      >
-        <div className="flex p-4">
-          <div className="h-5 w-5 rounded-md bg-gray-200" />
-          <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
-        </div>
-        <div className="flex items-center justify-center truncate rounded-xl bg-white px-4 py-8">
-          <div className="h-7 w-20 rounded-md bg-gray-200" />
-        </div>
-      </div>
+        className={`h-16 w-16 animate-pulse rounded-full bg-neutral-200 shadow-sm`}
+      ></div>
     )
   }
 
   return (
     <button
-      className="flex h-16 w-16 items-center gap-2 rounded-full border-2 border-gray-300 bg-transparent p-2 text-gray-800 shadow-sm transition-colors hover:bg-gray-200"
+      className={clsx(
+        'group relative m-16 h-16 w-16 items-center gap-2 overflow-hidden rounded-full border-2 shadow-sm transition-colors',
+        'border-neutral-300 hover:border-current dark:border-neutral-700 dark:hover:border-current',
+        'text-neutral-700 hover:text-current dark:text-neutral-300 dark:hover:text-current',
+      )}
       onClick={() => {
         let newTheme = resolvedTheme === 'light' ? 'dark' : 'light'
         if (systemTheme === newTheme) newTheme = 'system'
         setTheme(newTheme)
       }}
-    >
-      <SunIcon height={24} width={24} className="" />
-      <MoonIcon height={24} width={24} className="text-gray-800" />
-      <span className="sr-only">
-        {resolvedTheme === 'dark'
+      aria-label={
+        resolvedTheme === 'dark'
           ? 'Switch to Light theme'
-          : 'Switch to Dark theme'}
-      </span>
+          : 'Switch to Dark theme'
+      }
+    >
+      <div className="absolute left-0 top-0 h-16 w-16 bg-gradient-to-t from-red-300 via-orange-200 to-yellow-100 opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:group-hover:opacity-0"></div>
+      <div className="absolute left-0 top-0 h-16 w-16 bg-gradient-to-tl from-purple-950 via-indigo-800 to-sky-700 opacity-0 transition-opacity duration-500 dark:group-hover:opacity-100"></div>
+      <div className="absolute left-1/2 top-0 flex h-32 w-32 origin-center -translate-x-1/2 rotate-90 flex-row items-center justify-between transition-transform duration-500 dark:-rotate-90">
+        <div className="flex h-16 w-16 items-center justify-center">
+          <SunIcon height={24} width={24} />
+        </div>
+        <div className="flex h-16 w-16 items-center justify-center">
+          <MoonIcon height={24} width={24} />
+        </div>
+      </div>
     </button>
   )
 }
