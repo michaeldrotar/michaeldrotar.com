107ed79abf37fefafe0ca82a87054fa884121e4d
michaeldrotar <michaeldrotar@gmail.com>
2024-02-09 20:16:35 -0500

    Fix prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,9 @@ const styleguide = require('@vercel/style-guide/prettier')
 
 module.exports = {
   ...styleguide,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 }
