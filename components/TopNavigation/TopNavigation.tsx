f52327c18787f071bc280cf0fd7dc28b35290051
michaeldrotar <michaeldrotar@gmail.com>
2024-02-12 12:11:34 -0500

    Add logo and restrict top nav width

--- components/TopNavigation/TopNavigation.tsx
+++ components/TopNavigation/TopNavigation.tsx
@@ -1,10 +1,20 @@
 import { ThemeToggleButton } from '@/theme/ThemeToggleButton/ThemeToggleButton'
+import Image from 'next/image'
 
 export function TopNavigation() {
   return (
-    <nav className="flex h-32 items-center justify-between px-4">
+    <nav className="m-auto flex h-32 max-w-7xl items-center justify-between px-6">
       <div>
-        <h1 className="text-2xl font-bold">Michael Drotar</h1>
+        <h1 className="flex flex-row items-center gap-2 text-2xl font-bold">
+          <Image
+            src="/m-ribbon-logo.png"
+            height="32"
+            width="32"
+            alt=""
+            aria-hidden="true"
+          />
+          Michael Drotar
+        </h1>
       </div>
       <div>
         <ThemeToggleButton />
