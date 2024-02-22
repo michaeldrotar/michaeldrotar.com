011fae66a842324544df7fe78b0716ca17c1f115
michaeldrotar <michaeldrotar@gmail.com>
2024-02-21 23:12:08 -0500

    Add jest, update linting, update configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withBuilderDevTools = require('@builder.io/dev-tools/next')()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBuilderDevTools({})
+const nextConfig = withBuilderDevTools({
+  images: { domains: ['cdn.builder.io'] },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+})
 
 module.exports = nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,7 @@ const config: Config = {
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
     plugin(({ addUtilities, matchUtilities, theme }) => {
       matchUtilities(
         {
