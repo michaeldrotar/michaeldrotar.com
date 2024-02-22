a952070ac5b1c2cf5a7f0dd4524fc071f59425bc
michaeldrotar <michaeldrotar@gmail.com>
2024-02-22 13:12:08 -0500

    Ignore vue tests

--- jest.config.ts
+++ jest.config.ts
@@ -167,9 +167,10 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: [
+    // "\\\\node_modules\\\\"
+    'vue',
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
