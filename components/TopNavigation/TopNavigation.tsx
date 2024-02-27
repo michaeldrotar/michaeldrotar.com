dc9d09e70d9d83d768204e467e18ca442509c754
michaeldrotar <michaeldrotar@gmail.com>
2024-02-27 09:27:22 -0500

    Fix top nav z-index for mobile

--- components/TopNavigation/TopNavigation.tsx
+++ components/TopNavigation/TopNavigation.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export function TopNavigation() {
   return (
-    <nav className="relative z-10 m-auto flex h-32 max-w-7xl items-center justify-between px-6">
+    <nav className="relative z-40 m-auto flex h-32 max-w-7xl items-center justify-between px-6">
       <div>
         <Link href={'/'}>
           <h1 className="flex flex-row items-center gap-2 text-2xl font-bold">
