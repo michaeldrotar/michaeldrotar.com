327fa761d64c9bf2b54ede4bf5310f50e7376c6d
michaeldrotar <michaeldrotar@gmail.com>
2024-02-26 19:40:44 -0500

    Add link back to homepage

--- components/TopNavigation/TopNavigation.tsx
+++ components/TopNavigation/TopNavigation.tsx
@@ -1,20 +1,23 @@
 import { ThemeToggleButton } from '@/theme/ThemeToggleButton/ThemeToggleButton'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function TopNavigation() {
   return (
     <nav className="relative z-10 m-auto flex h-32 max-w-7xl items-center justify-between px-6">
       <div>
-        <h1 className="flex flex-row items-center gap-2 text-2xl font-bold">
-          <Image
-            src="/m-ribbon-logo.png"
-            height="32"
-            width="32"
-            alt=""
-            aria-hidden="true"
-          />
-          Michael Drotar
-        </h1>
+        <Link href={'/'}>
+          <h1 className="flex flex-row items-center gap-2 text-2xl font-bold">
+            <Image
+              src="/m-ribbon-logo.png"
+              height="32"
+              width="32"
+              alt=""
+              aria-hidden="true"
+            />
+            Michael Drotar
+          </h1>
+        </Link>
       </div>
       <div>
         <ThemeToggleButton />
